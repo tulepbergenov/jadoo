c956ae07c36872b8b96f8225fe65350ea2c42f83
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-12-29 17:19:25 +0600

    build: add image-optimizer

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,4 +1,3 @@
-import "normalize.css";
 import "../scss/_index.scss";
 
 console.log("Hello World");
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { ViteMinifyPlugin } from "vite-plugin-minify";
 import nunjucks from "vite-plugin-nunjucks";
 
 export default {
-  plugins: [nunjucks(), ViteMinifyPlugin({})],
+  plugins: [nunjucks(), ViteMinifyPlugin({}), ViteImageOptimizer({})],
 };
