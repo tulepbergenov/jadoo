eebe80ffd9f30fcfc28755e44ff0587a4d6331db
Daulet Tulepbergenov <daulet6368@gmail.com>
2023-12-29 22:05:31 +0600

    style: update header

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,23 @@
 import "../scss/_index.scss";
 
-console.log("Hello World");
+const langSwitcherBtn = document.getElementById("lang-switcher");
+const langSwitcherLangs = document.getElementById("lang-switcher-langs");
+
+if (langSwitcherBtn && langSwitcherLangs) {
+  langSwitcherBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    langSwitcherBtn.classList.toggle("lang-switcher__current-lang-btn_active");
+    langSwitcherLangs.classList.toggle("lang-switcher__list_show");
+  });
+
+  document.addEventListener("click", (event: MouseEvent) => {
+    const targetElement = event.target as HTMLElement;
+
+    if (!langSwitcherBtn.contains(targetElement)) {
+      langSwitcherBtn.classList.remove(
+        "lang-switcher__current-lang-btn_active",
+      );
+      langSwitcherLangs.classList.remove("lang-switcher__list_show");
+    }
+  });
+}
