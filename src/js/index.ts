3cef7427065efaeb1c320b477c7530c48d850510
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-01-12 22:39:08 +0600

    feat: update lang-picker

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,21 @@
 import "../scss/_index.scss";
+
+const langSwitcherBtn = document.getElementById("language-picker");
+const langSwitcherLangs = document.getElementById("language-picker-list");
+
+if (langSwitcherBtn && langSwitcherLangs) {
+  langSwitcherBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    langSwitcherBtn.classList.toggle("language-picker-active");
+    langSwitcherLangs.classList.toggle("language-picker-list-active");
+  });
+
+  document.addEventListener("click", (event: MouseEvent) => {
+    const targetElement = event.target as HTMLElement;
+
+    if (!langSwitcherBtn.contains(targetElement)) {
+      langSwitcherBtn.classList.remove("language-picker-active");
+      langSwitcherLangs.classList.remove("language-picker-list-active");
+    }
+  });
+}
